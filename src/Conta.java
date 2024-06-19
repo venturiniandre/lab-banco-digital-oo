75555aa4a0c52dacb6080fc1baa4f6c4f10c2483
André de Lucca Venturini <155122346+venturiniandre@users.noreply.github.com>
2024-06-19 17:45:47 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -44,6 +44,7 @@ public abstract class Conta implements IConta {
 	}
 
 	protected void imprimirInfosComuns() {
+		System.out.println(------------------------------------);
 		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
 		System.out.println(String.format("Agencia: %d", this.agencia));
 		System.out.println(String.format("Numero: %d", this.numero));
